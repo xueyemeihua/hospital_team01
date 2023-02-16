85df481eb07027bf5f497c95037ea2d5458c0f66
ly <2889533234@qq.com>
2023-02-16 16:40:57 +0800

    查询所有医生排班,挂号时根据医生当天排班进行筛选--李阳

--- src/main/java/org/kgc/controller/ShowStafOnDayServlet.java
+++ src/main/java/org/kgc/controller/ShowStafOnDayServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class ShowStafOnDayServlet extends HttpServlet {
@@ -19,7 +20,7 @@ public class ShowStafOnDayServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String date = request.getParameter("date");
         PaibanService paibanService = new PaibanService();
-        List<Paiban> paibans = paibanService.getPaibanByDate(date);
+        List<HashMap> paibans = paibanService.getStafsByGroupOnDept();
         HttpSession session = request.getSession();
         session.setAttribute("paibans",paibans);
 
