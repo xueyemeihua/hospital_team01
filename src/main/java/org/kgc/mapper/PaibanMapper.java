a22064d3f501ae916179bf748c9f6dbf5b574af3
ly <2889533234@qq.com>
2023-02-16 16:34:35 +0800

    根据医生排班查询值班医生

--- src/main/java/org/kgc/mapper/PaibanMapper.java
+++ src/main/java/org/kgc/mapper/PaibanMapper.java
@@ -1,7 +1,6 @@
 package org.kgc.mapper;
 
 import org.apache.ibatis.annotations.Param;
-import org.kgc.pojo.Paiban;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +11,24 @@ import java.util.List;
  */
 public interface PaibanMapper {
 
-    List<Paiban> getPaibanByDate(@Param("date")String date);
+    //查询周一某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanOneAndDeptid(@Param("deptid") Integer deptid);
+    //查询周二某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanTwoAndDeptid(@Param("deptid") Integer deptid);
+    //查询周三某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanThreeAndDeptid(@Param("deptid") Integer deptid);
+    //查询周四某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanFourAndDeptid(@Param("deptid") Integer deptid);
+    //查询周五某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanFiveAndDeptid(@Param("deptid") Integer deptid);
+    //查询周六某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanSixAndDeptid(@Param("deptid") Integer deptid);
+    //查询周日某个部门所有的有班医生
+    List<HashMap> getStafsByPaibanSevenAndDeptid(@Param("deptid") Integer deptid);
+
+
+    //按照部门排序查询所有医生排班
+    List<HashMap> getStafsByGroupOnDept();
 
 
 }
