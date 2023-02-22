96eb59e23c7481a200be443cf28b679e6ea27c8e
ly <2889533234@qq.com>
2023-02-22 15:36:52 +0800

    刘奇部分整理

---
+++ src/main/java/org/kgc/controller/ToSickViewServlet.java
@@ -0,0 +1,40 @@
+package org.kgc.controller; /**
+ * @author 雪夜梅花香_ly
+ * @create 2023-02-22-13:27
+ */
+
+import org.kgc.pojo.Sickerinfo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ToSickViewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        /*获取登录的病人信息*/
+        Sickerinfo loginSicker = (Sickerinfo) request.getSession().getAttribute("loginSicker");
+
+        if (loginSicker!=null){
+            //登录过,直接进入个人中心
+            request.getRequestDispatcher("").forward(request,response);
+
+        } else {
+            //跳转登录界面
+            request.getRequestDispatcher("/xylq/sicker_login.html").forward(request,response);
+
+        }
+
+
+
+
+    }
+}
--- src/main/java/org/kgc/service/ApplicantidService.java
+++ src/main/java/org/kgc/service/ApplicantidService.java
@@ -12,12 +12,12 @@ public class ApplicantidService {
     ApplicantidMapper mapper = sqlSession.getMapper(ApplicantidMapper.class);
 
 
-    public int insertApplicantid(Applicant applicant){
+    public int insertApplicantid(Applicant applicant) {
 
         int i = mapper.insertApplicant(applicant);
-        if(i!=0){
+        if (i != 0) {
             sqlSession.commit();
-        }else {
+        } else {
             sqlSession.rollback();
         }
         sqlSession.close();
@@ -25,21 +25,21 @@ public class ApplicantidService {
     }
 
     //根据id查科室
-    public String selectDeptName(int deptid){
+    public String selectDeptName(int deptid) {
         String s = mapper.selectDeptName(deptid);
         sqlSession.close();
         return s;
     }
 
     //查所有应聘信息
-    public List<Applicant> selectApplicants(){
+    public List<Applicant> selectApplicants() {
         List<Applicant> applicants = mapper.selectApplicants();
         sqlSession.close();
         return applicants;
     }
 
 
-    public int selectCarid(String carid){
+    public int selectCarid(String carid) {
         int i = mapper.selectCarid(carid);
         sqlSession.close();
         return i;
@@ -47,10 +47,26 @@ public class ApplicantidService {
 
 
     //根据岗位id查询应聘人信息
-    public List<Applicant> selectDeptApplicants(int postid){
+    public List<Applicant> selectDeptApplicants(int postid) {
         List<Applicant> applicants = mapper.selectDeptApplicants(postid);
         sqlSession.close();
         return applicants;
+    }
+
+
+    //查所有
+    public List<Applicant> selectSuccessfulApplicants() {
+        List<Applicant> applicants = mapper.SelectSuccessfulApplications();
+        sqlSession.close();
+        return applicants;
+    }
+
+    //模糊查询
+    public List<Applicant> FuzzySearchSuccessfulApplicationServlet(String name, String email) {
+        List<Applicant> applicantList = mapper.FuzzySearchSuccessfulApplicationServlet(name, email);
+        sqlSession.close();
+        return applicantList;
+
     }
 
 
